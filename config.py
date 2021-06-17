98f5c81ef08e0a5eebc9e898bc753392a4416647
Dima <61248439+Gorelo-v@users.noreply.github.com>
2021-06-17 22:41:15 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+
+TOKEN = '1889553278:AAFxnCBv-KvKbdwJ--PApkkAmYy0dTLG7-w'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+}
